c75b84655faeb02370458aae0b9394b26993e75b
tangbq <tangbq@tiens.com>
2017-09-06 11:14:15 +0800

    update from dell msysgit

--- Controller.php
+++ Controller.php
@@ -4,5 +4,29 @@ namespace qiyu;
 
 class Controller extends \yii\web\Controller
 {
+    public $layout = false;
 
+    public function render($tpl, $data = [])
+    {
+        if ($this->isJson()) {
+            return $this->renderJson(['tpl' => $tpl, 'data' => $data]);
+        } else {
+            return parent::render($tpl, $data);
+        }
+    }
+
+    public function renderJson($arr)
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        return $arr;
+    }
+
+    private function isJson()
+    {
+        if (isset($_GET['isJson']) && $_GET['isJson'] == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
