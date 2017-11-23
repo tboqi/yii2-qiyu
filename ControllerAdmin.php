f2d5896a7f0447a9937f11274a825863f7f714bb
tangbq <tangbq@tiens.com>
2017-11-23 13:56:30 +0800

    update

--- ControllerAdmin.php
+++ ControllerAdmin.php
@@ -4,13 +4,11 @@ namespace qiyu;
 
 use Yii;
 
-class ControllerAdmin extends Controller
-{
-    public function beforeAction($action)
-    {
+class ControllerAdmin extends Controller {
+    public function beforeAction($action) {
         parent::beforeAction($action);
         if (!Yii::$app->user->getId()) {
-            return $this->redirect('/account/login');
+            //return $this->redirect('/account/login');
         }
         return true;
     }
